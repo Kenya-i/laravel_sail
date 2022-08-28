bb82fa2dbc24e69d6a4a07b19ee05510c5563db9
Kenya Ishizuka <ken8116@icloud.com>
2022-08-28 15:26:48 +0900

    seeder作成

---
+++ database/seeders/ExampleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ExampleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('examples')->insert([
+            'id' => 10
+        ]);
+    }
+}
